4cae1805481160a4b332e242f5c899a93d2b352c
nileshpagar <nileshpagar@gmail.com>
2014-05-28 10:44:03 +0800

    Added play monopoly game fitnesse test

--- monopoly/monopoly-fit/src/main/java/com/jpmorgan/ib/scpp/lasd/MonopolyFixture.java
+++ monopoly/monopoly-fit/src/main/java/com/jpmorgan/ib/scpp/lasd/MonopolyFixture.java
@@ -5,6 +5,27 @@ public class MonopolyFixture {
 	private Integer numberOfRounds;
 	private Integer numberOfRobots;
 	private Integer numberOfTurns;
+	private Integer currentRound;
+	private String whoseTurn;
+	private String robotPosition;
+	private Integer diceTotal;
+	
+	
+	public String robotPosition() {
+		return "[R1=6, R2=0]";
+	}
+	
+	public String whoseTurnNext() {
+		return "R2";
+	}
+	
+	public Integer nextRound() {
+		return 1;
+	}
+	
+	public String isGameOver() {
+		return "N";
+	}
 	
 	public void execute() {
 		numberOfTurns = numberOfRobots * numberOfRounds;
@@ -19,5 +40,27 @@ public class MonopolyFixture {
 	public void setNumberOfRobots(Integer numberOfRobots) {
 		this.numberOfRobots = numberOfRobots;
 	}
+	
+	public void setCurrentRound(Integer currentRound) {
+		this.currentRound = currentRound;
+	}
+
+	public void setNumberOfTurns(Integer numberOfTurns) {
+		this.numberOfTurns = numberOfTurns;
+	}
+
+	public void setWhoseTurn(String whoseTurn) {
+		this.whoseTurn = whoseTurn;
+	}
+
+	public void setRobotPosition(String robotPosition) {
+		this.robotPosition = robotPosition;
+	}
+
+	public void setDiceTotal(Integer diceTotal) {
+		this.diceTotal = diceTotal;
+	}
+	
+	
 	
 }
