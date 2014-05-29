bfbbc8149e492aa6a7d1f5700d2f14e94fc99d09
nileshpagar <nileshpagar@gmail.com>
2014-05-29 15:27:45 +0800

    Merging PlayerTest

--- monopoly/monopoly-game/src/test/java/com/jpmorgan/ib/scpp/lasd/PlayerTest.java
+++ monopoly/monopoly-game/src/test/java/com/jpmorgan/ib/scpp/lasd/PlayerTest.java
@@ -1,32 +1,35 @@
 package com.jpmorgan.ib.scpp.lasd;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotSame;
-import static org.junit.Assert.assertNotEquals;
-
 public class PlayerTest{
 
-    private Player testObj;
+    private static final String ARBITRARY_PLAYER_ID = "arbitraryPlayerId";
+	private Player testObj;
 
-    @Test
-    public void initialize(){
-        assertEquals("playerId", testObj.getId());
-    }
-
-    @Before
+	@Before
     public void setUp() {
         testObj =  new Player("playerId");
+        testObj.setDie1(new Die());
+        testObj.setDie2(new Die());
+    }
+    
+	@Test
+    public void initialize(){
+        assertEquals(ARBITRARY_PLAYER_ID, testObj.getId());
     }
 
     @Ignore
     public void takeTurn_changesCurrentSquare(){
         int firstPosition = testObj.getCurrentSquare();
+        
         testObj.takeTurn();
+        
         int nextPosition = testObj.getCurrentSquare();
         assertNotEquals(firstPosition, nextPosition);
     }
