600a0eadd44443cae5aaa0e69c3baa17d10cd9ec
abhinav <abhinav@gmail.com>
2014-05-29 15:45:41 +0800

    added board class

---
+++ monopoly/monopoly-game/src/test/java/com/jpmorgan/ib/scpp/lasd/Square.java
@@ -0,0 +1,18 @@
+package com.jpmorgan.ib.scpp.lasd;
+
+public class Square {
+	
+	
+
+	public Square() {
+		
+	}
+
+	public int getCurrentPosition() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+	
+	
+
+}
