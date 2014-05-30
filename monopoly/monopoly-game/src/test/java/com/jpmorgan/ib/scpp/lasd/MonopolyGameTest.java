ac4cb1304a62c2d4b83204298ceddb9f2095ee40
nileshpagar <nileshpagar@gmail.com>
2014-05-30 14:44:25 +0800

    Fined the merge issue with monopoly test

--- monopoly/monopoly-game/src/test/java/com/jpmorgan/ib/scpp/lasd/MonopolyGameTest.java
+++ monopoly/monopoly-game/src/test/java/com/jpmorgan/ib/scpp/lasd/MonopolyGameTest.java
@@ -1,15 +1,11 @@
 package com.jpmorgan.ib.scpp.lasd;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-<<<<<<< HEAD
-
-=======
->>>>>>> 12cb192af27569e39ad0f58b6f1709b62fe2d8b5
 
 public class MonopolyGameTest {
 
@@ -24,51 +20,17 @@ public class MonopolyGameTest {
 
 	@Test
 	public void playGame() {
-		
-		testObject.playGame(NUMBER_OF_PLAYERS, NUMBER_OF_ROUNDS);
-<<<<<<< HEAD
-
-		assertEquals(NUMBER_OF_PLAYERS, testObject.players.size());
-		assertEquals(NUMBER_OF_ROUNDS, testObject.numberOfRounds);
-		assertNotNull(testObject.board);
-	}
-	
-	@Test
-	public void playGame_whenHaving3Rounds2Players_roundsCompleted() {
 		testObject.playGame(NUMBER_OF_PLAYERS, NUMBER_OF_ROUNDS);
 
-		assertEquals(NUMBER_OF_ROUNDS, testObject.roundsCompleted);
-	}
-	
-	@Test
-	public void playGame_whenHaving3Rounds2Players_currentRoundIsEqualToNumberOfRoundsAndEveryPlayerHasTakenTurn() {
-		testObject.playGame(NUMBER_OF_PLAYERS, NUMBER_OF_ROUNDS);
-
-		assertEquals(NUMBER_OF_ROUNDS, testObject.currentRound);
-=======
-		
 		assertEquals(NUMBER_OF_PLAYERS, testObject.players.size());
 		assertEquals(NUMBER_OF_ROUNDS, testObject.numberOfRounds);
 		assertNotNull(testObject.players.get(0).board);
 		assertNotNull(testObject.players.get(0).die1);
 		assertNotNull(testObject.players.get(0).die2);
-
->>>>>>> 12cb192af27569e39ad0f58b6f1709b62fe2d8b5
 		for (Player player : testObject.players) {
 			assertEquals(NUMBER_OF_ROUNDS, player.numberOfTurnsTaken);
 		}
-		
 		assertTrue(testObject.isGameOver);
 	}
 	
-<<<<<<< HEAD
-	
-	@Test
-	public void playGame_whenHaving3Rounds2Players_isGameOver() {
-		testObject.playGame(NUMBER_OF_PLAYERS, NUMBER_OF_ROUNDS);
-		assertTrue(testObject.isGameOver);
-	}
 }
-=======
-}
->>>>>>> 12cb192af27569e39ad0f58b6f1709b62fe2d8b5
